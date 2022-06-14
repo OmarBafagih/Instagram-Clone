031894122428aff4ce9e85bf02a89ee06bfe2f99
Omar Bafagih <bafagihomar260@gmail.com>
2022-06-14 10:15:44 -0700

    Users can now log out

--- app/src/main/java/com/example/instagram/Timeline.java
+++ app/src/main/java/com/example/instagram/Timeline.java
@@ -3,15 +3,36 @@ package com.example.instagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.parse.ParseUser;
 
 public class Timeline extends AppCompatActivity {
+
+   // BottomNavigationView bottomNavigationView;
+
+    Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
 
+       // bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_nav_view);
 
+        //getting reference to log out button and defining it's onClick listener
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                finish();
+            }
+        });
 
     }
 }
